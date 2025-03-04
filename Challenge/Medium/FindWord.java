7a773e28861a4813f99c97025ec856e7912cbc5c
Yamlak Negash <yamlaknegash96@gmail.com>
2025-03-04 20:39:06 +0300

    Updated Challenge files.

--- Challenge/Medium/FindWord.java
+++ Challenge/Medium/FindWord.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class FindWord {
 
   public static String censorWord(String sentence, String word){
-    
+    return sentence.replaceAll(word, "*".repeat(word.length()));//NOT MINE.
   }
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
