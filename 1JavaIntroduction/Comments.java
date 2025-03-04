029ac18c31b297f978926261ca6a3eddbd6e6437
Yamlak Negash <yamlaknegash96@gmail.com>
2025-03-04 11:58:55 +0300

    Intro files added.

---
+++ 1JavaIntroduction/Comments.java
@@ -0,0 +1,8 @@
+public class Comments {
+    public static void main(String[] args) {
+        // This is a single line comment
+        System.out.println("Hello, World!"); // This is also a single line comment
+        /* This is a multi-line comment
+        that spans multiple lines */
+    }
+}
