7f99c04b89a7728a2c7bfccbffac3c7fec00c329
Yamlak Negash <yamlaknegash96@gmail.com>
2025-03-04 13:31:35 +0300

    Updated the 2nd folder.

---
+++ 2JavaFundamentals/Literals.java
@@ -0,0 +1,33 @@
+public class Literals {
+  public static void main(String[] args) {
+    // Integer Literals
+    int myInt = 100;
+    System.out.println(myInt); // Output: 100
+
+    // Floating Point Literals
+    double myDouble = 3.14;
+    System.out.println(myDouble); // Output: 3.14
+
+    // Character Literals
+    char myChar = 'A';
+    System.out.println(myChar); // Output: A
+    char myUnicodeChar = '\u0041';
+    System.out.println(myUnicodeChar); // Output: A
+    char sth = '\b';
+    System.out.println(sth); // Output: Backspace
+    sth = '\t';
+    System.out.println(sth); // Output: Tab
+    sth = '\n';
+    System.out.println(sth); // Output: New Line
+    sth = '\f'; // Form Feed
+    System.out.println(sth); // Output: Form Feed
+
+    // String Literals
+    String myString = "Hello, World!";
+    System.out.println(myString); // Output: Hello, World!
+
+    // Boolean Literals
+    boolean myBool = true;
+    System.out.println(myBool); // Output: true
+  }
+}
---
+++ 2JavaFundamentals/Variables.java
@@ -0,0 +1,30 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declare a variable of type int
+        int myNumber;
+        // Assign a value to the variable
+        myNumber = 5;
+        // Print the value of the variable
+        System.out.println(myNumber); // Output: 5
+
+        // Declare a variable of type double
+        double myDouble = 5.99;
+        // Print the value of the variable
+        System.out.println(myDouble); // Output: 5.99
+
+        // Declare a variable of type char
+        char myLetter = 'D';
+        // Print the value of the variable
+        System.out.println(myLetter); // Output: D
+
+        // Declare a variable of type boolean
+        boolean myBool = true;
+        // Print the value of the variable
+        System.out.println(myBool); // Output: true
+
+        // Declare a variable of type String
+        String myText = "Hello";
+        // Print the value of the variable
+        System.out.println(myText); // Output: Hello
+    }
+}
