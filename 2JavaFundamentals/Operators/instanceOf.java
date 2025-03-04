2cec68e66bcd2429556543af4b6439b17bd327a4
Yamlak Negash <yamlaknegash96@gmail.com>
2025-03-04 15:11:09 +0300

    Updated 2nd Folder

---
+++ 2JavaFundamentals/Operators/instanceOf.java
@@ -0,0 +1,12 @@
+public class instanceOf {
+  public static void main(String[] args) {
+
+    String str = "Programiz";
+    boolean result;
+
+    // checks if str is an instance of
+    // the String class
+    result = str instanceof String;
+    System.out.println("Is str an object of String? " + result);
+  }
+}
---
+++ 2JavaFundamentals/Operators/ternary.java
@@ -0,0 +1,11 @@
+public class ternary {
+  public static void main(String[] args) {
+
+    int februaryDays = 29;
+    String result;
+
+    // ternary operator
+    result = (februaryDays == 28) ? "Not a leap year" : "Leap year";
+    System.out.println(result);
+  }
+}
