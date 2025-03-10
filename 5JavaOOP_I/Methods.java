e3a440df955a9bf88973ea97035be92423781786
Yamlak Negash <yamlaknegash96@gmail.com>
2025-03-11 00:41:10 +0300

    Fifth Folder Update

--- 5JavaOOP_I/Methods.java
+++ 5JavaOOP_I/Methods.java
@@ -1,5 +1,12 @@
 public class Methods {
   public static void main(String[] args) {
-    
+    /*
+     * Important Points
+Two or more methods can have the same name inside the same class if they accept different arguments. This feature is known as method overloading.
+Method overloading is achieved by either:
+changing the number of arguments.
+or changing the data type of arguments.
+It is not method overloading if we only change the return type of methods. There must be differences in the number of parameters.
+     */
   }
 }
